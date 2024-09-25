8f9b0d09f1e4917853882cede2edbcb7bcc7a3d8
Marcel Schmidt <marcelschmidt1989@live.de>
2024-09-25 11:48:26 +0200

    chore: Add initial template for detail-evolution component
    - Added a placeholder paragraph to indicate the component is working.

---
+++ src/app/components/detail-evolution/detail-evolution.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-evolution',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-evolution.component.html',
+  styleUrl: './detail-evolution.component.scss'
+})
+export class DetailEvolutionComponent {
+
+}
