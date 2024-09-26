beac8fcda6594e8306da984b30bea6751a0495a8
Marcel Schmidt <marcelschmidt1989@live.de>
2024-09-26 18:32:27 +0200

    feat: Add search-bar component
    Adds the search-bar component to the project. The component includes an HTML template and a TypeScript file. It is a standalone component and can be used independently.

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.scss'
+})
+export class SearchBarComponent {
+
+}
