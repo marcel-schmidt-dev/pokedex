a6318b4dc3dc60b82df6f6649ddc1b647070253c
Marcel Schmidt <marcelschmidt1989@live.de>
2024-09-26 17:32:31 +0200

    add: Update detail-evolution component to display evolution chain with pokemon name and additional details

--- src/app/components/detail-evolution/detail-evolution.component.ts
+++ src/app/components/detail-evolution/detail-evolution.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PokemonService } from '../../services/pokemon.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detail-evolution',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detail-evolution.component.html',
   styleUrl: './detail-evolution.component.scss',
 })
@@ -14,8 +15,8 @@ export class DetailEvolutionComponent {
   evolutionChain: any;
   species: any;
   evolutions: {
-    species_name: string;
-    species_sprite: string;
+    name: string;
+    sprite: string;
     level_to_evolve: number | null;
     stone: string | null;
   }[] = [];
@@ -50,8 +51,8 @@ export class DetailEvolutionComponent {
         const evolutionDetails = chain.evolution_details[0] || {};
         const item = evolutionDetails.item ? evolutionDetails.item.name : null;
         evolutions.push({
-          species_name: chain.species.name,
-          species_sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${speciesId}.png`,
+          name: chain.species.name,
+          sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${speciesId}.png`,
           level_to_evolve: evolutionDetails.min_level || null,
           stone: item,
         });
